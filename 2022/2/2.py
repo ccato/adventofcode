66a7b62a5cc5b846bc53b86256af3da537100ca1
Christopher Cato <christopher.cato@forefront.se>
2022-12-02 10:29:52 +0100

    Day 2 solutions

---
+++ 2022/2/2.py
@@ -0,0 +1,18 @@
+def fixLine(line):
+    wins = {'A':'Y','B':'Z','C':'X'}
+    draws = {'A':'X','B':'Y','C':'Z'}
+    losses = {'A':'Z','B':'X','C':'Y'}
+    moves = line.split(' ')
+    if moves[1] == 'X':
+        return "%s %s" % (moves[0],losses.get(moves[0]))
+    if moves[1] == 'Y':
+        return "%s %s" % (moves[0],draws.get(moves[0]))
+    else:
+        return "%s %s" % (moves[0],wins.get(moves[0]))
+
+with open("input.txt") as file:
+    score = 0
+    scoring = {'A X': 4,'A Y': 8,'A Z': 3,'B X': 1,'B Y': 5,'B Z': 9,'C X': 7,'C Y': 2,'C Z': 6 }
+    lines = file.read()
+    lines = list(map(lambda x: fixLine(x), lines.split('\n')))
+    print(sum(list(map(lambda x: scoring.get(x), lines))))
