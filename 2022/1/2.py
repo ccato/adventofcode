d77f259146c8ad73f379dd734e72460ed6c03135
Christopher Cato <christopher.cato@forefront.se>
2022-12-02 08:34:12 +0100

    adding solutions

---
+++ 2022/1/2.py
@@ -0,0 +1,6 @@
+counts = list()
+with open("input.txt") as elfcals:
+    for elfline in elfcals.read().split('\n\n'):
+        counts.append(sum(int(x) for x in elfline.split('\n')))
+    counts.sort(reverse=True)
+    print(counts[0])
